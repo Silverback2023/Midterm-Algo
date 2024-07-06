c2aca739dec80cba3565cdca62139c3c19c209e8
Silverback2023 <121978344+Silverback2023@users.noreply.github.com>
2024-07-05 17:28:29 -0700

    Update twoNumberReturnIndices.js

---
+++ twoNumberReturnIndices.js
@@ -0,0 +1,25 @@
+function generateAllPermutations(str) {
+    const results = [];
+
+    // Helper to do recursion
+    function permute(arr, l, r) {
+        if (l === r) {
+            results.push(arr.join(''));
+        } else {
+            for (let i = l; i <= r; i++) {
+                [arr[l], arr[i]] = [arr[i], arr[l]]; 
+                permute(arr, l + 1, r);
+                [arr[l], arr[i]] = [arr[i], arr[l]]; 
+            }
+        }
+    }
+
+    permute(str.split(''), 0, str.length - 1);
+    return results;
+}
+
+console.log(generateAllPermutations("NBA"));   // Expected output: ["NBA", "NAB", "BNA", "BAN", "ABN", "ANB"]
+console.log(generateAllPermutations("BA"));    // Expected output: ["BA", "AB"]
+"CBAD", "CBDA", "CABD", "CADB", "CDAB", "CDBA", "DBCA", "DBAC", "DCBA", "DCAB", "DACB", "DABC"]
+
+Overall time complexity O(n⋅n!)
